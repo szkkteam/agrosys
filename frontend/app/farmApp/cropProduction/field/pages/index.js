d7d7ce51f18eb44e8544e1ffdf34d6c9e0a1a625
Istvan Rusics <szkkteam1@gmail.com>
2021-01-19 17:31:24 +0100

    Minor cleanup

--- frontend/app/farmApp/cropProduction/field/pages/index.js
+++ frontend/app/farmApp/cropProduction/field/pages/index.js
@@ -1,4 +1,3 @@
-export { default as FieldCreateDraw } from './FieldCreateDraw'
 export { default as FieldProductionList } from './FieldProductionList'
 
 export { default as FieldSeasonCreate } from './FieldSeasonCreate'
